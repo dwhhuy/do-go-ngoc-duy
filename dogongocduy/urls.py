d25e6d7f2a755e9dbdf15d4703e41180eb7ec2fd
huy-do <dwhhuy@tma.com.vn>
2019-04-10 11:28:49 +0700

    done

--- dogongocduy/urls.py
+++ dogongocduy/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from dogo import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin', admin.site.urls),
     url(r'', views.home)
 ]
