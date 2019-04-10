fa2985eea7e5ed2e1ae51811e97c6b0481636ac7
huy-do <dwhhuy@tma.com.vn>
2019-04-10 10:46:20 +0700

    abc

--- dogongocduy/settings.py
+++ dogongocduy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,18 +72,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dogongocduy.wsgi.application'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd35gjvg46tj2ui',
-        'USER': 'pqcyngxjznaemn',
-        'PASSWORD': 'a8f6c1cfdf20066fbac6e907bc28a4d9f5371009381b6ad6f6c768efd91787f6',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
